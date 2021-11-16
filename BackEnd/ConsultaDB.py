d9dfdb87f3c9709000c3ed4fb7a662b5eb284d0e
Paulo Gabriel <38271933+NvewTT@users.noreply.github.com>
2021-11-16 15:24:52 -0300

    Adicionado novo endPoint para validação do login do usuario, criação da função para validação do login do usuario

--- BackEnd/ConsultaDB.py
+++ BackEnd/ConsultaDB.py
@@ -1,6 +1,11 @@
 import mysql.connector
 from mysql.connector import errorcode
 
+def resultados(status, sucesso):
+    return {
+        'status': status,
+        'sucesso': sucesso
+        }
 
 def insereUsuario(usuario: str, senha: str):
     status = ""
@@ -20,13 +25,24 @@ def insereUsuario(usuario: str, senha: str):
         mydb.commit()
     except mysql.connector.IntegrityError as error:
         status = error.msg
-        return {
-            'status': status,
-            'sucesso': False
-            }
+        return resultados(status,False)
     status = "Usuario inserido com sucesso"
-    return {
-        'status': status,
-        'sucesso': True
-        }
+    return resultados(status,True)
+
+def validacaoUsuario(usuario,senha):
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        database="gamdb"
+        )
+
+    mycursor = mydb.cursor()
+
+    sql = "SELECT CASE WHEN EXISTS  (SELECT * FROM `usuarios` WHERE Usuario = %s AND Senha = %s)THEN '1' ELSE '0' END"
+    val = (usuario, senha)
+    mycursor.execute(sql, val)
+    resultado = mycursor.fetchone()
+    if resultado[0] == '1':
+        return resultados("usuário existe no banco de dados", True)
+    return resultados("usuário não existe no banco de dados", False)
 
--- BackEnd/RestApiGAM.py
+++ BackEnd/RestApiGAM.py
@@ -1,17 +1,22 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from ConsultaDB import *
+
 app = Flask(__name__)
 api = Api(app)
 
 
 class CriarUsuario(Resource):
     def get(self, usuario, senha):
+        return insereUsuario(usuario, senha)
 
-        return insereUsuario(usuario,senha)
+
+class ValidacaoLogin(Resource):
+    def get(self, usuario, senha):
+        return validacaoUsuario(usuario, senha)
 
 
 api.add_resource(CriarUsuario, '/criarUsuario/<string:usuario>:<string:senha>')
-
+api.add_resource(ValidacaoLogin, '/login/<string:usuario>:<string:senha>')
 if __name__ == '__main__':
     app.run(debug=True)
